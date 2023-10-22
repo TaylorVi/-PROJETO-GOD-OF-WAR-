4c7f440cbce4a1f76573157665aabb7badfe26c6
TaylorVi <taylor.vieira.77@gmail.com>
2023-10-22 11:20:06 -0300

    second commit

--- js/main.js
+++ js/main.js
@@ -2,4 +2,30 @@ var swiper = new Swiper(".slide-character", {
     slidesPerView: 3.5,
     spaceBetween: 19,
     freeMode: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      420: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 2.2,
+      },
+      991: {
+        slidesPerView: 2.8,
+      },
+      1200: {
+        slidesPerView: 3,
+      },
+      1440: {
+        slidesPerView: 3.5,
+      },
+    }
+  });
+
+  AOS.init({
+    duration: 1000,
+    once: true,
+    
   });
